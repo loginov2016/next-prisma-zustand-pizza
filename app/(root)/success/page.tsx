868ef6858311e4d1dd26a6626acdce0265a37357
Andrey_Loginov <myhomemail2015@mail.ru>
2025-10-01 14:28:21 +0500

    Commit #55. Bug fixing in the success/page.tsx.

--- app/(root)/success/page.tsx
+++ app/(root)/success/page.tsx
@@ -2,13 +2,9 @@ import { InfoBlock } from '@/components/shared';
 import { cn } from '@/lib/utils';
 import React from 'react';
 
-interface ISuccessPageProps {
-  className?: string;
-}
-
-export default function SuccessPage({ className }: ISuccessPageProps) {
+export default function SuccessPage() {
   return (
-    <div className={cn('flex flex-col items-center justify-center my-40', className)}>
+    <div className={cn('flex flex-col items-center justify-center my-40')}>
         <InfoBlock 
             title='Оплата успешна произведена!'
             text='Вернитесь на Главную страницу'
