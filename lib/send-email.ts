71862d06c471805dca103974a124724e51577bc2
Andrey_Loginov <myhomemail2015@mail.ru>
2025-10-03 18:06:47 +0500

    Commit #63. In the sendMail function I set up my domain email.

--- lib/send-email.ts
+++ lib/send-email.ts
@@ -5,10 +5,8 @@ import { Resend } from 'resend';
 export const sendEmail = async ( to: string, subject: string, template: React.ReactNode) => {
     const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
-    //console.log('API_KEYS:___', resend.apiKeys.list().then(res => console.log(res)));
-
     const { data, error } = await resend.emails.send({
-            from: 'onboarding@resend.dev',
+            from: 'loginov_av_2025@super-pizza.site',
             to,
             subject,
             text: '',
